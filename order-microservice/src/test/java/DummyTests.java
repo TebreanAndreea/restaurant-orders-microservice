01e0c58f86435b3644e7dee36ac03a1fdb8da526
Dorian Herbiet <d.n.g.herbiet@student.tudelft.nl>
2023-12-05 14:43:26 +0100

    First dummy test

---
+++ order-microservice/src/test/java/DummyTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTests {
+
+    @Test
+    public void dummyTest(){
+        int x = 6 * 7;
+        Assertions.assertEquals(42, x);
+    }
+}
