52dadc1e24edddca6ccaa6f46c732b9acc317327
Dorian Herbiet <d.n.g.herbiet@student.tudelft.nl>
2023-12-05 15:14:49 +0100

    Fixing checkstyle (first of a long series)

--- order-microservice/src/test/java/DummyTests.java
+++ order-microservice/src/test/java/DummyTests.java
@@ -3,8 +3,11 @@ import org.junit.jupiter.api.Test;
 
 public class DummyTests {
 
+    /**
+     * Dummy test to verify that the pipeline is working properly.
+     */
     @Test
-    public void dummyTest(){
+    public void dummyTest() {
         int x = 6 * 7;
         Assertions.assertEquals(42, x);
     }
