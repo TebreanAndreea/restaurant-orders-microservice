0f179ba95a3158a7782fe8352580ef68f8ec51d0
Claudia Scorţia <c.scortia@student.tudelft.nl>
2024-01-07 16:15:47 +0100

    Modified the place of the implementation of withinRadius

--- order-microservice/src/main/java/nl/tudelft/sem/yumyumnow/database/VendorRepository.java
+++ order-microservice/src/main/java/nl/tudelft/sem/yumyumnow/database/VendorRepository.java
@@ -14,7 +14,25 @@ public interface VendorRepository extends JpaRepository<Vendor, Long> {
     List<Vendor> findByVendorNameContaining(@Param("filter") String filter);
 
     @Query("SELECT v FROM Vendor v WHERE v.name LIKE %:filter% "
-        + "AND FUNCTION('withinRadius', v.location, :location, :radius) = true")
+        + "AND FUNCTION('withinRadius', :location, :radius, v.location) = true")
     List<Vendor> findByLocationWithinRadius(@Param("location") Location location, @Param("filter") String filter,
         @Param("radius") Integer radius);
+
+    /**
+     * Checks if the given address is in the given radius of the vendor's address.
+     *
+     * @param address The address to check.
+     * @param radius The radius to check.
+     * @param vendorAddress The vendor's address.
+     * @return True if the address is in the radius, false otherwise.
+     */
+    default boolean withinRadius(Location address, Integer radius, Location vendorAddress) {
+        if (address == null || vendorAddress == null || radius == null || radius < 0) {
+            return false;
+        }
+        return Math.acos(Math.sin(address.getLatitude()) * Math.sin(vendorAddress.getLatitude())
+            + Math.cos(address.getLatitude()) * Math.cos(vendorAddress.getLatitude())
+            * Math.cos(address.getLongitude() - vendorAddress.getLongitude())) * 6371 <= (double) radius / 1000.0;
+    }
+
 }
--- order-microservice/src/test/java/nl/tudelft/sem/yumyumnow/database/TestVendorRepository.java
+++ order-microservice/src/test/java/nl/tudelft/sem/yumyumnow/database/TestVendorRepository.java
@@ -292,23 +292,6 @@ public class TestVendorRepository implements VendorRepository {
         return found;
     }
 
-    /**
-     * Checks if the given address is in the given radius of the vendor's address.
-     *
-     * @param address The address to check.
-     * @param radius The radius to check.
-     * @param vendorAddress The vendor's address.
-     * @return True if the address is in the radius, false otherwise.
-     */
-    public boolean withinRadius(Location address, Integer radius, Location vendorAddress) {
-        if (address == null || vendorAddress == null || radius == null || radius < 0) {
-            return false;
-        }
-        return Math.acos(Math.sin(address.getLatitude()) * Math.sin(vendorAddress.getLatitude())
-                + Math.cos(address.getLatitude()) * Math.cos(vendorAddress.getLatitude())
-                * Math.cos(address.getLongitude() - vendorAddress.getLongitude())) * 6371 <= (double) radius / 1000.0;
-    }
-
     @Override
     public List<Vendor> findByLocationWithinRadius(@Param("location") Location location, @Param("filter") String filter,
         @Param("radius") Integer radius) {
