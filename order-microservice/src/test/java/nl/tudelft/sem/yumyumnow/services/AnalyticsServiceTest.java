5bacb5819f0933436ef65aac8b881987b9220e36
Stoica Ruxandra <R.Stoica@student.tudelft.nl>
2024-01-12 23:23:42 +0100

    some more tests

--- order-microservice/src/test/java/nl/tudelft/sem/yumyumnow/services/AnalyticsServiceTest.java
+++ order-microservice/src/test/java/nl/tudelft/sem/yumyumnow/services/AnalyticsServiceTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import nl.tudelft.sem.yumyumnow.database.RatingRepository;
 import nl.tudelft.sem.yumyumnow.database.TestRatingRepository;
 import nl.tudelft.sem.yumyumnow.model.Dish;
 import nl.tudelft.sem.yumyumnow.model.Rating;
@@ -14,13 +13,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-
 public class AnalyticsServiceTest {
 
     private TestRatingRepository testRatingRepository;
-    private RatingRepository ratingRepository;
     private AnalyticsService analyticsService;
-    private AnalyticsService mockedAnalyticsService;
     private VendorService vendorService;
     private OrderService orderService;
 
@@ -30,11 +26,9 @@ public class AnalyticsServiceTest {
     @BeforeEach
     public void setup() {
         this.vendorService = Mockito.mock(VendorService.class);
-        this.ratingRepository = new TestRatingRepository();
         this.orderService = Mockito.mock(OrderService.class);
         this.testRatingRepository = new TestRatingRepository();
         this.analyticsService = new AnalyticsService(vendorService, testRatingRepository, orderService);
-        this.mockedAnalyticsService = new AnalyticsService(vendorService, ratingRepository, orderService);
     }
 
     @Test
@@ -85,18 +79,18 @@ public class AnalyticsServiceTest {
 
         Mockito.when(vendorService.getVendorDishes(vendorId)).thenReturn(dishes);
 
-        assertEquals(average, mockedAnalyticsService.getAverageVendorPrice(vendorId));
+        assertEquals(average, analyticsService.getAverageVendorPrice(vendorId));
     }
 
     @Test
     void testGetAverageVendorPriceOneDish() {
         Long vendorId = 1L;
         Dish dish = new Dish().id(2L).price(23.4);
-        List<Dish> dishes = Arrays.asList(dish);
+        List<Dish> dishes = Collections.singletonList(dish);
 
         Mockito.when(vendorService.getVendorDishes(vendorId)).thenReturn(dishes);
 
-        assertEquals(23.4, mockedAnalyticsService.getAverageVendorPrice(vendorId));
+        assertEquals(23.4, analyticsService.getAverageVendorPrice(vendorId));
     }
 
     @Test
@@ -105,7 +99,51 @@ public class AnalyticsServiceTest {
 
         Mockito.when(vendorService.getVendorDishes(vendorId)).thenReturn(Collections.emptyList());
 
-        assertEquals(null, mockedAnalyticsService.getAverageVendorPrice(vendorId));
+        assertEquals(null, analyticsService.getAverageVendorPrice(vendorId));
+    }
+
+    @Test
+    void testGetAverageVendorRating() {
+        Long vendorId = 1L;
+        Long id = testRatingRepository.count();
+        Rating rating1 = new Rating().id(id).grade(2L);
+        Rating rating2 = new Rating().id(id + 1).grade(3L);
+        Rating rating3 = new Rating().id(id + 2).grade(5L);
+
+        List<Rating> ratings = Arrays.asList(rating1, rating2, rating3);
+        List<Long> ratingsIds = Arrays.asList(rating1.getId(), rating2.getId(), rating3.getId());
+
+        testRatingRepository.saveAll(ratings);
+
+        Mockito.when(orderService.getAllRatingsForVendor(vendorId)).thenReturn(ratingsIds);
+
+        Double average = (double) (rating1.getGrade() + rating2.getGrade() + rating3.getGrade()) / 3;
+        assertEquals(average, analyticsService.getAverageVendorRating(vendorId));
+    }
+
+    @Test
+    void testGetAverageVendorRatingOneRating() {
+        Long vendorId = 1L;
+        Long id = testRatingRepository.count();
+        Rating rating = new Rating().id(id).grade(2L);
+
+        List<Rating> ratings = Collections.singletonList(rating);
+        List<Long> ratingsIds = Collections.singletonList(rating.getId());
+
+        testRatingRepository.saveAll(ratings);
+
+        Mockito.when(orderService.getAllRatingsForVendor(vendorId)).thenReturn(ratingsIds);
+
+        assertEquals((double) rating.getGrade(), analyticsService.getAverageVendorRating(vendorId));
+    }
+
+    @Test
+    void testGetAverageVendorRatingNoRatings() {
+        Long vendorId = 1L;
+
+        Mockito.when(orderService.getAllRatingsForVendor(vendorId)).thenReturn(Collections.emptyList());
+
+        assertEquals(null, analyticsService.getAverageVendorRating(vendorId));
     }
 }
 
